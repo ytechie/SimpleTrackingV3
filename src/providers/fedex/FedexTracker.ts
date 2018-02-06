3fe3fa6a3fc7efbe740716f05165241cb4058303
Jason Young <jason@ytechie.com>
2018-02-06 12:45:18 -0800

    Fedex tracker working

--- src/providers/fedex/FedexTracker.ts
+++ src/providers/fedex/FedexTracker.ts
@@ -1,6 +1,8 @@
 import * as request from 'request';
+import * as xml2js from 'xml2js';
 import { TrackingData } from '../TrackingData';
 import { ITracker } from '../ITracker';
+import { ActivityData } from '../ActivityData';
 
 export class FedexTracker implements ITracker {
     FEDEX_REQUEST_TEMPLATE = '<TrackRequest xmlns=\'http://fedex.com/ws/track/v3\'><WebAuthenticationDetail><UserCredential><Key>{key}</Key>'
@@ -12,7 +14,7 @@ export class FedexTracker implements ITracker {
         + '<IncludeDetailedScans>1</IncludeDetailedScans></TrackRequest>';
 
     FEDEX_API_URL = "https://gatewaybeta.fedex.com/xml";
-    //FEDEX_API_URL = "https://requestb.in/1oijy051";
+    //FEDEX_API_URL = "https://ws.fedex.com/web-services";
 
     key:string;
     password:string;
@@ -35,45 +37,69 @@ export class FedexTracker implements ITracker {
             .replace('{trackingNumber}', trackingNumber);
     }
 
-    Track(trackingNumber:string) {
+    async Track(trackingNumber:string):Promise<TrackingData> {
         return new Promise<TrackingData>((resolve, reject) => {
-            //resolve(null); //Don't return results until implemented
-
             let req = this.buildRequest(trackingNumber);
 
             let options = {
-                    json:false,
-                    strictSSL: false,
+                    //json:false,
+                    //strictSSL: false,
                     body:req,
                     headers:
                         {
-                          'Content-Type': 'text/xml',
-                            'User-Agent': 'SimpleTracking/3.0.0',
-                            'Accept': '*/*',
-                            'Host': 'gatewaybeta.fedex.com:443', //<--- this was the problem!!!
-                            'Cache-Control': 'no-cache',
-                            'Cookie': '__cfduid=d8d6b4b20c6f3eaa49fb13f829000c1921517882609',
-                            'Connection': 'close'
+                            'Host': 'gatewaybeta.fedex.com:443',
                         }
-            }
+            };
 
-            console.log(req);
+            console.log('requesting fedex data');
             request.post(this.FEDEX_API_URL, options, (error, response, body) => {
                 if(error) {
                     console.log("Error in Fedex tracker request: " + error);
                     reject("Error in Fedex tracker request: " + error);
+                    return;
                 }
 
-                let td = FedexTracker.ConvertResponseToStandardFormat(body);
-                resolve(td);
+                console.log('got fedex data');
+                let parser = new xml2js.Parser({explicitArray: false});
+                parser.parseString(body, (err, result) => {
+                    if(err) {
+                        reject(err);
+                    } else {
+                        resolve(this.ConvertJsonToStandardFormat(result));
+                    }
+                });
             });
         });
     }
 
-    private static ConvertResponseToStandardFormat(response:string):TrackingData {
-        let td = new TrackingData();
+    private ConvertJsonToStandardFormat(results:any):TrackingData {
+        //Check for invalid tracking number code
+        if(results.TrackReply.Notifications && results.TrackReply.Notifications.Code === "6035") {
+            console.log('Fedex says they have no data for the package');
+            return null;
+        }
 
-        console.log(response);
+        let td = new TrackingData();
+        td.activity = new Array<ActivityData>();
+        let trackDetail = results.TrackReply.TrackDetails;
+        td.trackingNumber = trackDetail.TrackingNumber;
+        td.weight = trackDetail.PackageWeight.Value + trackDetail.PackageWeight.Units;
+        if(trackDetail.ActualDeliveryTimestamp) {
+            td.estimatedDelivery = new Date(Date.parse(trackDetail.ActualDeliveryTimestamp));
+        }
+        trackDetail.Events.forEach((event) => {
+            var ad = new ActivityData();
+            ad.timestamp = new Date(Date.parse(event.Timestamp));
+            ad.locationDescription = event.Address.City;
+            ad.shortDescription = event.EventDescription;
+            td.activity.push(ad)
+        });
+
+        td.usageRequirements = "NOTICE: FedEx authorizes you to use FedEx tracking"
+            + " systems solely to track shipments tendered by"
+            + " or for you to FedEx for delivery and for no other"
+            + " purpose. Any other use of FedEx tracking systems and"
+            + " information is strictly prohibited.";
 
         return td;
     }
