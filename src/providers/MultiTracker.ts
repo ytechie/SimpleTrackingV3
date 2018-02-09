1880f366bffaebb54383699c1fe78cb6903a97b0
Jason Young <jason@ytechie.com>
2018-02-09 12:31:14 -0800

    Better logging in multitracker

--- src/providers/MultiTracker.ts
+++ src/providers/MultiTracker.ts
@@ -9,24 +9,32 @@ export class MultiTracker implements ITracker {
     }
 
     async Track(trackingNumber: string): Promise<TrackingData> {
-        return new Promise<TrackingData>((resolve) => {
+        return new Promise<TrackingData>((resolve, reject) => {
             var foundMatch = false;
             var resolveCount = 0;
             //Return the results from the first tracker that responds with data
             this.trackers.forEach((tracker) => {
+                let remainingTrackers = this.trackers;
                 tracker.Track(trackingNumber).then((trackingData) => {
+                    console.log('Tracker ' + tracker.constructor.name + ' responded');
                     resolveCount++;
                     if(!foundMatch && trackingData) {
                         foundMatch = true;
+                        console.log('Found a tracker with data');
                         resolve(trackingData);
                     }
                     if(resolveCount === this.trackers.length) {
                         resolve(null);
                     }
                 }).catch((error) => {
+                    console.error('Tracker ' + tracker.constructor.name + ' responded with error ' + error);
                     resolveCount++;
-                    console.error(error);
-                });;
+
+                    //If the last tracker errors, we still need to respond upstream
+                    if(resolveCount === this.trackers.length) {
+                        resolve(null);
+                    }
+                });
             });
         });
     }
