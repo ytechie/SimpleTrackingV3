d07f27f9a194687875d0ed8b6cb23e581ace5b61
Jason Young <jason@ytechie.com>
2018-02-13 16:55:25 -0800

    Complete geocoding + caching

--- sqlite/commands.sql
+++ sqlite/commands.sql
@@ -4,9 +4,18 @@
 --CSV downloaded from https://simplemaps.com/data/us-cities
 
 .mode csv
-.import uscitiesv1.3.csv uscities
+.import uscitiesv1.3.csv uscities_temp
+
+BEGIN TRANSACTION;
+CREATE TABLE uscities(city, state_id, state_name, lat, lng, new);
+INSERT INTO uscities SELECT city, state_id, state_name, lat, lng, '0' FROM uscities_temp;
+DROP TABLE uscities_temp;
+COMMIT;
 
 CREATE INDEX city_index ON uscities (city);
+
+CREATE TABLE otherlocations(location, lat, lng, new);
+CREATE INDEX otherlocations_index ON otherlocations (location);
 
 select count(*) from uscities;
 
--- src/geocoder.ts
+++ src/geocoder.ts
@@ -19,36 +19,89 @@ export class Geocoder {
     }
 
     async Geocode(location:Location) {
-        return new Promise<void>((resolve, reject) => {
-            if(location.city && location.state) {
-                this.db.all('select * from uscities where city=$city COLLATE NOCASE and state_id=$state;',
-                { $city: location.city, $state: location.state },
-                (err, rows) => {
+        console.time('Geocode');
+        try {
+        if(await this.TryLocalGeocode(location)) {
+            return;
+        }
+        if(await this.TryGeocodeWithGoogle(location)) {
+            await this.AddToLocalCache(location);
+            return;
+        }
+        } catch(err) {
+            console.warn('Could not geocode location');
+        }
+        console.timeEnd('Geocode');
+    }
+
+    async TryLocalGeocode(location:Location):Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            if((location.city && location.state) || location.rawLocationString) {
+                let sql='';
+                let sqlParams:any = {};
+                if(location.city && location.state) {
+                    sql = 'select * from uscities where city=$city COLLATE NOCASE and state_id=$state;';
+                    sqlParams.$city = location.city;
+                    sqlParams.$state = location.state;
+                } else {
+                    sql = 'select * from otherlocations where location=$location;';
+                    sqlParams.$location = location.rawLocationString;
+                }
+
+                this.db.all(sql, sqlParams, (err, rows) => {
                     //If there is more than 1 row, it's ambiguous  
                     if(rows && rows.length === 1) {
                         location.lat = rows[0].lat;
                         location.long = rows[0].lng;
-                        resolve();
-                    } else if(location.rawLocationString) {
-                        this.GeocodeWithGoogle(location.toString()).then((googleLoc) => {
-                            if(googleLoc) {
-                                location.lat = googleLoc[0];
-                                location.long = googleLoc[1];
-                            }
-                            
-                            resolve();
-                        });
+                        resolve(true);
                     } else {
-                        resolve();
+                        resolve(false);
                     }
                 });
-            } else if(location.rawLocationString) {
-                this.GeocodeWithGoogle(location.toString()).then((googleLoc) => {
-                    if(googleLoc) {
-                        location.lat = googleLoc[0];
-                        location.long = googleLoc[1];
-                    }
+            } else {
+                resolve(false);
+            }
+        });
+    }
 
+    async AddToLocalCache(location:Location):Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            if(location.city && location.state) {
+                let sqlParams:any = {};
+                if(location.state.length > 2) {
+                    sqlParams.$state_name = location.state;
+                } else {
+                    sqlParams.$state_id = location.state;
+                }
+
+                sqlParams.$city = location.city;
+                sqlParams.$lat = location.lat;
+                sqlParams.$lng = location.long;
+                sqlParams.$new = 1;
+                
+                let sql = Geocoder.GetInsertStatement('uscities', sqlParams);
+
+                let result = this.db.run(sql, sqlParams, (err) => {
+                    if(err) {
+                        console.error(err);
+                        reject(err);
+                    }
+                    resolve();
+                });
+            } else if(location.rawLocationString) {
+                let sqlParams:any = {};
+                sqlParams.$location = location.rawLocationString;
+                sqlParams.$new = 1;
+                sqlParams.$lat = location.lat;
+                sqlParams.$lng = location.long;
+
+                let sql = Geocoder.GetInsertStatement('otherlocations', sqlParams);
+
+                let result = this.db.run(sql, sqlParams, (err) => {
+                    if(err) {
+                        console.error(err);
+                        reject(err);
+                    }
                     resolve();
                 });
             } else {
@@ -57,24 +110,40 @@ export class Geocoder {
         });
     }
 
-    async 
+    static GetInsertStatement(tableName:string, sqlParams:any) {
+        let columns = new Array<string>();                
+        for(let key of Object.keys(sqlParams)) {
+            columns.push(key.replace('$',''));
+        }
+        let sql = 'insert into ' + tableName + ' ';
+        sql += '(' + columns.join(',') + ')';
+        sql += ' values($' +  columns.join(',$') + ')';
 
-    async GeocodeWithGoogle(address:string) {
-        return new Promise<[number,number]>((resolve, reject) => {
+        return sql;
+    }
+
+    async TryGeocodeWithGoogle(location:Location) {
+        return new Promise<boolean>((resolve, reject) => {
             //http://maps.googleapis.com/maps/api/geocode/json?address=DALLAS/FT. WORTH A/P, TX, US
 
-            let url = this.googleGeocodeUrl.replace('{address}', address);
+            let url = this.googleGeocodeUrl.replace('{address}', location.toString());
+
+            console.log('Geocoding ' + location.toString() + ' with Google');
 
             request.get(url, {json:true}, (error, response, body) => {
                 try { //Try catch is needed inside the request
                     if(error) {
                         console.log("Error in Google geocoder: " + error);
                         reject("Error in Google geocoder request: " + error);
+                        resolve(false);
                         return;
                     }
 
                     let loc = body.results[0].geometry.location;
-                    resolve([loc.lat, loc.lng]);
+                    location.lat = body.results[0].geometry.location.lat;
+                    location.long = body.results[0].geometry.location.lng;
+
+                    resolve(true);
                 } catch(err) {
                     reject(err);
                 }
